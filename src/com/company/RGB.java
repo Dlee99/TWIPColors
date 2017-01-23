436e6ba00a86f157eb94d3db659a4f018d85de55
Lee, Derek <Dereklee9ab@aol.com>
2017-01-23 13:34:55 -0500

    Update RGB.java

--- src/com/company/RGB.java
+++ src/com/company/RGB.java
@@ -12,28 +12,54 @@ public class RGB {
         r = Integer.parseInt(RGB[0]);
         g = Integer.parseInt(RGB[1]);
         b = Integer.parseInt(RGB[2]);
-        String R = "", G = "", B = "";
+        String R = Hexa(r), G = Hexa(g), B = Hexa(b);
+
+    }
+    public static String Hexa(int r){
+        String R = ¨¨;
         if(r % 16 < 10){
             R = R + (r % 16);
         }
-        else if(r % 16 = 10){
+        else if(r % 16 == 10){
             R = R + ¨A¨;
         }
-        else if(r % 16 = 11){
+        else if(r % 16 == 11){
             R = R + ¨B¨;
         }
-        else if(r % 16 = 12){
+        else if(r % 16 == 12){
             R = R + ¨C¨;
         }
-        else if(r % 16 = 13){
+        else if(r % 16 == 13){
             R = R + ¨D¨;
         }
-        else if(r % 16 = 14){
+        else if(r % 16 == 14){
             R = R + ¨E¨;
         }
+        else if(r % 16 == 15){
+            R = R + ¨F¨;
+        }
         r = r - (r % 16 * 16);
-        G = R + (g % 16);
-        g = g - (g % 16 * 16)
-
+        if(r < 10){
+            R = R + (r % 16);
+        }
+        else if(r == 10){
+            R = R + ¨A¨;
+        }
+        else if(r == 11){
+            R = R + ¨B¨;
+        }
+        else if(r == 12){
+            R = R + ¨C¨;
+        }
+        else if(r == 13){
+            R = R + ¨D¨;
+        }
+        else if(r == 14){
+            R = R + ¨E¨;
+        }
+        else if(r == 15){
+            R = R + ¨F¨;
+        }
+        return R;
     }
 }

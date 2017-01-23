b9bb546b4c6c3dbef695299b4221fb2bec8a1129
Lee, Derek <Dereklee9ab@aol.com>
2017-01-23 13:19:07 -0500

    Update RGB.java

--- src/com/company/RGB.java
+++ src/com/company/RGB.java
@@ -13,9 +13,27 @@ public class RGB {
         g = Integer.parseInt(RGB[1]);
         b = Integer.parseInt(RGB[2]);
         String R = "", G = "", B = "";
-        R = R + (r % 16);
-        r = r - r % 16;
+        if(r % 16 < 10){
+            R = R + (r % 16);
+        }
+        else if(r % 16 = 10){
+            R = R + ¨A¨;
+        }
+        else if(r % 16 = 11){
+            R = R + ¨B¨;
+        }
+        else if(r % 16 = 12){
+            R = R + ¨C¨;
+        }
+        else if(r % 16 = 13){
+            R = R + ¨D¨;
+        }
+        else if(r % 16 = 14){
+            R = R + ¨E¨;
+        }
+        r = r - (r % 16 * 16);
         G = R + (g % 16);
+        g = g - (g % 16 * 16)
 
     }
 }
